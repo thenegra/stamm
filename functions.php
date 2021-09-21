52958c8a80e49dccc007372a0a7d1df58d8e2c00
thenegra <ssanchez@thenegra.com>
2021-09-21 18:34:00 -0300

    asdf

--- functions.php
+++ functions.php
@@ -144,3 +144,8 @@ return count( $comments_by_type['comment'] );
 return $count;
 }
 }
+add_action('get_header', 'my_filter_head');
+
+  function my_filter_head() {
+    remove_action('wp_head', '_admin_bar_bump_cb');
+  }
