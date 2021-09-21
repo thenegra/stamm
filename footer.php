d4f0921f862f9dfec90b47f356c8f33563d459bc
thenegra <ssanchez@thenegra.com>
2021-09-21 17:39:10 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,8 +1,7 @@
-<?php get_sidebar(); ?>
+<?php //get_sidebar(); ?>
 </div>
 <footer id="footer" role="contentinfo">
 <div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
 </div>
 </footer>
 </div>
