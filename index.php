d041ac7f19efbfbe3fb43bc3d545542ba9dc2b2c
thenegra <ssanchez@thenegra.com>
2021-09-21 18:02:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 	<h1>Hola!</h1>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
-<?php comments_template(); ?>
+<?php //comments_template(); ?>
 <?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
 </main>
 <?php get_footer(); ?>
