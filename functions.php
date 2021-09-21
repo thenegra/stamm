3954fafce5b1cc7ad8f5bc66f3979408075f9f7f
thenegra <ssanchez@thenegra.com>
2021-09-21 18:24:41 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -25,6 +25,7 @@ add_user_meta( $user_id, 'blankslate_notice_dismissed_3', 'true', true );
 }
 add_action( 'wp_enqueue_scripts', 'blankslate_enqueue' );
 function blankslate_enqueue() {
+	wp_enqueue_style('style',get_template_directory().'/dist/css/styles.css')
 wp_enqueue_style( 'blankslate-style', get_stylesheet_uri() );
 wp_enqueue_script( 'jquery' );
 }
