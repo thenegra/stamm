7740299c40817775861887e2d2e3248af37983e3
thenegra <ssanchez@thenegra.com>
2021-09-30 19:10:10 -0300

    asdf

--- template-parts/block/content-mod-big-hero.php
+++ template-parts/block/content-mod-big-hero.php
@@ -5,12 +5,12 @@ $fotos = get_field('imagenes_feature');
 <section class="hero big-hero f-<?php echo get_field('diseno')['fondo']; ?>" style="background-image:url('<?php echo $fotos['fondo']['url'];?>')">
 	<div class="main-container">
 		<div class="contenedor-principal inner-padding" style="background-image:url('<?php echo $fotos['base']['url'];?>')">
-			<h1 class="mega">Internet<br>of life.</h1>		
-			<?php if(get_field('secundario')):?>
-			<span class="mini-text"><?php echo get_field('secundario'); ?></span>
+			<h1 class="mega"><?php echo get_field('titulo');?></h1>		
+			<?php if(get_field('bajada')):?>
+			<div class="bajada"><?php echo get_field('bajada'); ?></div>
 			<?php endif; ?>
 		</div>
-		<div class="contenedor-secundario" style="background-image:url('<?php echo $fotos['detalle']['url'];?>')"></div>
+		<div class="contenedor-secundario" style="background-image:url('<?php echo get_field('imagen')['url'];?>')"></div>
 	</div>
 	
 </section>
