2a61576ac7fb39d5e04162c8f5b5c47b21b5b221
thenegra <ssanchez@thenegra.com>
2021-10-04 16:33:38 -0300

    asdf
    afsd

--- template-parts/block/content-mod-big-hero.php
+++ template-parts/block/content-mod-big-hero.php
@@ -4,7 +4,7 @@ $fotos = get_field('imagenes_feature');
 </div>
 <section class="hero big-hero f-<?php echo get_field('diseno')['fondo']; ?>" style="background-image:url('<?php echo get_field('foto')['url'];?>')">
 	<div class="main-container">
-		<div class="contenedor-principal inner-padding" style="background-image:url('<?php echo $fotos['base']['url'];?>')">
+		<div class="contenedor-principal" style="background-image:url('<?php echo $fotos['base']['url'];?>')">
 			<h1 class="mega"><?php echo get_field('titulo');?></h1>		
 			<?php if(get_field('bajada')):?>
 			<div class="bajada tit-dos"><?php echo get_field('bajada'); ?></div>
