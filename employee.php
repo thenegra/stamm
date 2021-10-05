098182ea7e9311020a9fb3022454471aa2fc2d89
thenegra <ssanchez@thenegra.com>
2021-10-05 16:03:36 -0300

    Update employee.php

--- employee.php
+++ employee.php
@@ -1,7 +1,7 @@
 <article id="employee-<?php the_ID(); ?>" <?php post_class('employee'); ?>>
 	<div class="foto">
 	<?php if ( has_post_thumbnail() ) : 
-		the_post_thumbnail('feed-size',array('title'=>get_the_title()));
+		the_post_thumbnail('feed-size',array('alt'=>get_the_title(),'title'=>get_the_title()));
 	else:
 	endif;
 	?>
