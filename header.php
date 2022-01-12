74d381ab10dd574c3d02f39ff6d9bb8a2d9811e0
thenegra <ssanchez@thenegra.com>
2022-01-12 17:53:45 -0300

    Update header.php

--- header.php
+++ header.php
@@ -10,6 +10,15 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <?php wp_head(); ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-7H7JT68P7K"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7H7JT68P7K');
+</script>
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
