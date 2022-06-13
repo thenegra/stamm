61f3f22da5353372da40a4aec812d4191583cd26
thenegra <ssanchez@thenegra.com>
2022-06-13 15:13:39 -0300

    asdf
    asdf

--- template-parts/block/content-mod-adjunto.php
+++ template-parts/block/content-mod-adjunto.php
@@ -4,7 +4,7 @@
 		<div class="col">
 			<h2 class="tit-dos"><?php echo get_field('titulo'); ?></h2>	
 			<?php //var_dump(get_field('bajada'));
-			if(get_field('bajada')):?><p class="sbutit"><?php echo get_field('bajada');?></p><?php endif; ?>
+			if(get_field('bajada')):?><p class="subtit"><?php echo get_field('bajada');?></p><?php endif; ?>
 		</div>
 		<div class="col inner-padding big-padding">
 			<a href="<?php echo get_field('boton')['archivo']; ?>" class="boton" target="_blank"><?php echo get_field('boton')['texto']; ?></a>
