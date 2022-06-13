e71d06d7ba181848d6519446256cb48a6672e966
thenegra <ssanchez@thenegra.com>
2022-06-13 14:22:26 -0300

    asdf
    asdf

--- template-parts/block/content-mod-adjunto.php
+++ template-parts/block/content-mod-adjunto.php
@@ -1,12 +1,12 @@
 </div>
 <section class="modulo-adjunto f-<?php echo get_field('diseno')['fondo']; ?>">
 	<div class="main-container">
-		<div class="columna inner-padding big-padding">
+		<div class="columna">
 			<h2 class="tit-dos"><?php echo get_field('titulo'); ?></h2>	
 			<?php //var_dump(get_field('bajada'));
 			if(get_field('bajada')):?><p class="sbutit"><?php echo get_field('bajada');?></p><?php endif; ?>
 		</div>
-		<div class="columna inner-padding big-padding boton">
+		<div class="columna inner-padding big-padding">
 			<a href="<?php echo get_field('boton')['archivo']; ?>" class="boton" target="_blank"><?php echo get_field('boton')['texto']; ?></a>
 		</div>
 
