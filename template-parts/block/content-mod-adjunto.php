1de34b4498af426e9281a37faeb4ddc610c94b41
thenegra <ssanchez@thenegra.com>
2022-06-13 15:16:22 -0300

    Update content-mod-adjunto.php

--- template-parts/block/content-mod-adjunto.php
+++ template-parts/block/content-mod-adjunto.php
@@ -2,9 +2,9 @@
 <section class="modulo-adjunto f-<?php echo get_field('diseno')['fondo']; ?>">
 	<div class="main-container">
 		<div class="col">
-			<h2 class="tit-dos"><?php echo get_field('titulo'); ?></h2>	
+			<h2 class="tit-tres"><?php echo get_field('titulo'); ?></h2>	
 			<?php //var_dump(get_field('bajada'));
-			if(get_field('bajada')):?><p class="subtit"><?php echo get_field('bajada');?></p><?php endif; ?>
+			if(get_field('bajada')):?><p class="subtit-dos"><?php echo get_field('bajada');?></p><?php endif; ?>
 		</div>
 		<div class="col">
 			<a href="<?php echo get_field('boton')['archivo']; ?>" class="boton" target="_blank"><i class="fa fa-download"></i> <?php echo get_field('boton')['texto']; ?></a>
