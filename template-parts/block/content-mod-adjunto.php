0e5e5c948a2ebfcbc15fb1e1ce9e04059798c295
thenegra <ssanchez@thenegra.com>
2022-06-13 15:20:12 -0300

    asdf
    as

--- template-parts/block/content-mod-adjunto.php
+++ template-parts/block/content-mod-adjunto.php
@@ -1,7 +1,7 @@
 </div>
 <section class="modulo-adjunto f-<?php echo get_field('diseno')['fondo']; ?>">
 	<div class="main-container">
-		<div class="col">
+		<div class="col text">
 			<h2 class="tit-tres"><?php echo get_field('titulo'); ?></h2>	
 			<?php //var_dump(get_field('bajada'));
 			if(get_field('bajada')):?><p class="subtit-dos"><?php echo get_field('bajada');?></p><?php endif; ?>
