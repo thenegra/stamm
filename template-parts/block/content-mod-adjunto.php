b2c545694c12fb5a0a35700f9fbe1e2d34c92a5e
thenegra <ssanchez@thenegra.com>
2022-06-13 14:12:21 -0300

    Update content-mod-adjunto.php

--- template-parts/block/content-mod-adjunto.php
+++ template-parts/block/content-mod-adjunto.php
@@ -3,7 +3,8 @@
 	<div class="main-container">
 		<div class="columna inner-padding big-padding">
 			<h2 class="tit-dos"><?php echo get_field('titulo'); ?></h2>	
-			<?php var_dump(get_field('bajada'));if(get_field('bajada')):?><p class="sbutit"><?php echo get_field('bajada');?></p><?php endif; ?>
+			<?php //var_dump(get_field('bajada'));
+			if(get_field('bajada')):?><p class="sbutit"><?php echo get_field('bajada');?></p><?php endif; ?>
 		</div>
 		<div class="columna inner-padding big-padding boton">
 			<?php var_dump(get_field('boton')); ?>
